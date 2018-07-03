ef9fb9a4a6cf3b8f908209f36cbc0db29ddde6fb
Sujith Suresan <sujith7c@gmail.com>
2018-07-03 16:38:03 +0400

    Added the config file

--- conf/appConf.js
+++ conf/appConf.js
@@ -1,33 +1,38 @@
-var config = {
-development: {
-    //url to be used in link generation
-    url: 'http://my.site.com',
-    //mongodb connection settings
-    database: {
-        host:   '10.41.10.117',
-        port:   '27017',
-        db:     'inapp-dev'
-    },
-    //server details
-    server: {
-        host: '127.0.0.1',
-        port: '3422'
-    }
-},
-production: {
-    //url to be used in link generation
-    url: 'http://my.site.com',
-    //mongodb connection settings
-    database: {
-        host: '127.0.0.1',
-        port: '27017',
-        db:     'site'
-    },
-    //server details
-    server: {
-        host:   '127.0.0.1',
-        port:   '3421'
-    }
-}
+
+/*
+*
+* create and export configuration variables
+*
+*/
+
+
+//Environment container //
+
+var environments = {};
+
+//Environment variables for staging
+
+environments.staging = {
+    'envName' : "staging",
+    'port' : '4000',
 };
-module.exports = config;
+
+//Environment variables for production 
+
+environments.production  = {
+    'envName' : "production",
+    'port' : '5000'   
+};
+
+//set the environment passed to commandline
+
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+//determine which environment to passs to commandline
+
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+//expor the module
+
+module.exports = environmentToExport;
+
