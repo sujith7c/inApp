5c4a49044dea1cd5df14193d202638ff97d1218b
Sujith Suresan <sujith7c@gmail.com>
2018-07-25 16:59:05 +0400

    Bread crub fix

--- libs/common.js
+++ libs/common.js
@@ -13,7 +13,22 @@ var url = require('url');
                 host :  req.get('host'),
             }
      );
+     //get the url parts
      urlParts = url.parse(req.url, true).pathname.split('/');
+     items = [];
+
+     //Insert a home link as the url parts does'nt have 
+     items.push({label: 'Home', url : baseUrl})
+     //iterate till url parts length
+     //l = urlParts.length;
+     console.log(urlParts.length);
+     for(i = 0, l = urlParts.length; i < l ; i++) {
+        items.push(
+            {
+                label:  urlParts[i].replace(/-/g,' '),
+                url:    url.resolve(baseUrl, urlParts.slice(0,i+1).join('/')),
+            });
+     }
      console.log(urlParts);
  }
 module.exports = common;
