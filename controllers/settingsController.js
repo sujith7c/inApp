12aa0808a938b085b463d48a0e5a3df03ed426e5
Sujith Suresan <Sujith Suresan sujith7c@gmail.com>
2018-07-28 21:50:48 +0400

    Removed commented code

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -1,39 +1,15 @@
+/**
+ *  Config page controller
+ */
 var appSettings = {};
 
 settingsArr = [];
 appSettings.getEntityItems = function(req, res, next) {
     //we need to check from which page it being called
     //Array of configurable items in the App
-   
-    /*
-
-    settingsArr['conf'] = [
-        'Companay',
-        'Asset Type',
-        'Location',
-        'Store',
-        'Brand',
-        'User',
-        'Misc1',
-        'Locale',
-        'test1',
-    ];
-    settingsArr['columnCount'] = 2;
-    //Calculate numbe for row ;and cloume in settings page
-    if (parseInt(settingsArr['conf'].length / settingsArr['columnCount']) % settingsArr['columnCount'] == 0 ) {
-        settingsArr['rowCount'] = settingsArr['conf'].length / settingsArr['columnCount'];
-    }
-    else {
-        settingsArr['rowCount'] =  settingsArr['conf'].length / settingsArr['columnCount'] + 1;
-    }
-    settingsArr['items'] = _createSettings(settingsArr['conf'], settingsArr['rowCount'], settingsArr['columnCount']);
-    console.log(settingsArr['items']);
-    */
-
     settingObj = this.createConfigObj();
     console.log(settingObj);
     return settingObj;
-
     next();
 }
 /**
